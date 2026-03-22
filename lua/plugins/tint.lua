71dfba26c662e01000c500e28c6305e01975edb5
Halsten <leeser.lionel@gmail.com>
2026-03-22 16:19:37 +0100

    Better tint

--- lua/plugins/tint.lua
+++ lua/plugins/tint.lua
@@ -2,7 +2,7 @@ return {
   "levouh/tint.nvim",
   event = "WinNew",
   opts = {
-    tint = -30, -- how much to dim (-100 = black, 0 = no change)
+    tint = -55, -- how much to dim (-100 = black, 0 = no change)
     saturation = 0.7, -- reduce saturation in inactive windows
   },
 }
