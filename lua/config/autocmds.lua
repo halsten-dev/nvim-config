43f02dba9638736d24244f773e836ee2131479f4
Halsten <leeser.lionel@gmail.com>
2026-04-04 19:36:03 +0200

    Fixed auto import on save

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -12,3 +12,10 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.formatoptions:remove({ "c", "r", "o" })
   end,
 })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+    vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })
+  end,
+})
