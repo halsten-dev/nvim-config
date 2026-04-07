a8b78331f852f6694cda32c90041a673fa58430f
Halsten <leeser.lionel@gmail.com>
2026-04-07 20:58:27 +0200

    Actually not that usefull

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -12,10 +12,3 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.formatoptions:remove({ "c", "r", "o" })
   end,
 })
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-    vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })
-  end,
-})
