506426be6ef420fae5499de2993f4601749169ef
Halsten <leeser.lionel@gmail.com>
2026-07-22 19:03:30 +0200

    Delete lua directory

--- lua/plugins/tint.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "levouh/tint.nvim",
-  event = "WinNew",
-  opts = {
-    tint = -55, -- how much to dim (-100 = black, 0 = no change)
-    saturation = 0.7, -- reduce saturation in inactive windows
-  },
-}
